225e9bdd034f1dfba5d244450efbceff4a203afc
medusa717 <77543020+medusa717@users.noreply.github.com>
2025-06-09 00:24:03 +0300

    feat: add throttling support with @nestjs/throttler for rate limiting

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,19 +1,39 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ProductsModule } from './products/products.module';
-import { ResponseInterceptor } from './common/interceptors/transform-response.interceptor';
 import { ShippingModule } from './shipping/shipping.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ResponseInterceptor } from './common/interceptors/transform-response.interceptor';
 
 @Module({
-  imports: [UsersModule, ProductsModule, AuthModule, OrdersModule, CartModule, ShippingModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+    UsersModule,
+    ProductsModule,
+    AuthModule,
+    OrdersModule,
+    CartModule,
+    ShippingModule,
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
